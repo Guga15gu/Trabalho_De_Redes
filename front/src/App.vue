31f70268f250e400f858fd7046fc7878cddb9d85
Leticia Garcez <lpgarcez@inf.ufpel.edu.br>
2021-09-13 00:04:53 -0300

    criacao do projeto

---
+++ front/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Botao cor="#F1C453" tecla = "A"/>
+    <Botao cor="#F1C453" tecla = "S"/>
+  </div>
+</template>
+
+<script>
+import Botao from './components/Botao.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Botao
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  align-content: space-between;
+}
+</style>
